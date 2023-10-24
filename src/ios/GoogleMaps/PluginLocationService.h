8c052e1964124e473e59584dde7d435e98f32fb7
CoeOmnichannel <73339509+CoeOmnichannel@users.noreply.github.com>
2023-10-24 15:07:21 +0100

    Update PluginLocationService.h

--- src/ios/GoogleMaps/PluginLocationService.h
+++ src/ios/GoogleMaps/PluginLocationService.h
@@ -21,6 +21,8 @@
 
 - (void)getMyLocation:(CDVInvokedUrlCommand*)command;
 - (void)hasPermission:(CDVInvokedUrlCommand*)command;
-- (void)getSuggestionsFromLocations:(NSString *)textLocation country:(NSString *)country:(CDVInvokedUrlCommand *)command;
+//- (void)getSuggestionsFromLocations:(NSString *)textLocation country:(NSString *)country callbackContext:(CDVInvokedUrlCommand *)command;
+//- (void)getSuggestionsFromLocations:(NSString *)textLocation country:(NSString *)country:(CDVInvokedUrlCommand *)command;
+- (void)getSuggestionsFromLocations:(NSString *)textLocation country:(NSString *)country callbackContext:(CDVInvokedUrlCommand *)command;
 
 @end
